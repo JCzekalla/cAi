63e3d78affba762a0e1e78b64967d6c9e08e790a
JCzekalla <71197728+JCzekalla@users.noreply.github.com>
2022-04-04 23:35:15 -0400

    Delete AiLibrary.h

--- AiLibrary.h
+++
@@ -1,8 +0,0 @@
-#ifndef _AiLibrary_h
-#define _AiLibrary_h
-
-Network* initMyNetwork(int* model);
-void trainMyNetwork(Network* network,int epochCount, double** inputs,double** desired);
-Result* runMyNetwork(Network* network,double* inputs);
-
-#endif
